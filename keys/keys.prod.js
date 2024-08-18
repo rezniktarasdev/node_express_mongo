effb148cac9ad99d55f2c285f5b0199bd170f2b4
rezniktarasdev <75927896+rezniktarasdev@users.noreply.github.com>
2024-08-18 13:12:27 +0300

    update keys.prod file

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,9 @@
 module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
+    BREVO_SMTP_SERVER: process.env.BREVO_SMTP_SERVER,
+    BREVO_SMTP_PORT: process.env.BREVO_SMTP_PORT,
+    BREVO_SMTP_LOGIN: process.env.BREVO_SMTP_LOGIN,
     BREVO_SMTP_KEY: process.env.BREVO_SMTP_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
     BASE_URL: process.env.BASE_URL
